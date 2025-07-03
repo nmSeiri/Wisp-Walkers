60aa4d3329a0bbf5a63d1eabf86a4760d0ba55f8
nmSeiri <lostinrika@gmail.com>
2025-07-03 14:11:06 +0200

    First changes

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed = 5f;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Gameplay Ideas and Lore/Storia.md
+++ Gameplay Ideas and Lore/Storia.md
@@ -1,3 +1,4 @@
 TBD
 
 ### Personaggi 
+Remi - 
