cf72b085084a19dcc65f2801ae42add796a122fe
nmSeiri <lostinrika@gmail.com>
2025-07-04 00:29:35 +0200

    Player Base Movements (To improve)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,16 +3,42 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+
     public float speed = 5f;
-    
+    public float jumpForce = 12f;
+
+    private bool isGrounded;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundLayer;
+    private float groundCheckRadius = 0.1f;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    [System.Obsolete]
     void Update()
     {
-        
+        // horizontal movement
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        // Checks if the character is touching grass (always false for you) >:)
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        // Jump 
+        // TO DO - Stop jump when spacebar isn't pressed anymore
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+    }
+    
+    void OnDrawGizmosSelected()
+    {
+        if (groundCheck != null)
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
